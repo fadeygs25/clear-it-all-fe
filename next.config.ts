96fe2beeaf8f9c51232a898c5ddffdb99a46ab0a
Dung Nguyen <nqdd251003@gmail.com>
2024-10-28 04:03:20 +0700

    [Feature] Update config

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,12 @@
-import type { NextConfig } from "next";
+// next.config.ts
+import type { NextConfig } from 'next';
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  reactStrictMode: true,
+  swcMinify: true,     
+  images: {
+    domains: ['cdn.builder.io'], 
+  },
 };
 
 export default nextConfig;
