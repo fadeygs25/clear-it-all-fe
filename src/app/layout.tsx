beb03fa8c2883f5afd10a14c6fb70045be91813d
Dung Nguyen <nqdd251003@gmail.com>
2024-10-28 07:34:55 +0700

    [Feature] Delete Anuphan

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -19,7 +19,7 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body className={`antialiased ${anuphan.className}`}>
+      <body className="antialiased">
         {children}
       </body>
     </html>
